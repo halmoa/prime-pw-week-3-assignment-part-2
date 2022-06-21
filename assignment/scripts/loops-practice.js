97478e1dc8f7de720e158aa1b374a640f7ebe68c
Halima Moalim <halimaamoalim@gmail.com>
2022-06-21 11:49:52 -0500

    while loops comp frozen

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -30,8 +30,8 @@ for(let i =0;i< evenNumbers.length; i++){
 }
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
-for(let i=5;i<=0;i--){
-  console.log(i);
+for(let i=5;i>=0;i--){
+  console.log('Lets CountDown',i);
 }
 
 // 2. For of loops  
@@ -49,6 +49,7 @@ console.log('---- 3. While loop ----');
 
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
 console.log('Some stars using while:');
+
 
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
